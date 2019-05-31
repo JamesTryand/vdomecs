2bb2afce0a25e15324d52badf0640b94933550d7
James Tryand <james.tryand@informationcatalyst.com>
2019-05-31 14:44:06 +0100

    fixed the tests for throws

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,4 +1,4 @@
-import {expect} from "chai";
+import {expect, should} from "chai";
 import createEntity from "../src/createEntity";
 import createComponent from "../src/createComponent";
 
@@ -19,6 +19,21 @@ describe("a component",() => {
         it("should have a state which is an object",() => {
             const result = createComponent();
             expect(result).to.have.property("state").and.be.a("object");
+        });
+    });
+});
+describe("Systems",() => {
+    const createSystem = (component) => {
+        if(!component || component === undefined) {
+            throw 'components missing'; 
+        }
+        
+        console.log("system created");
+    }
+    describe("creating a new system", ()=>{
+        it("should require a component ",() => {
+            expect(() => {createSystem()}).to.throw();
+
         });
     });
 });
